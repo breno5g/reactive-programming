4773a56384ffe623be56aadb6636f28484a5d68b
Breno Santos <brenosantos145@gmail.com>
2024-08-19 22:42:17 -0300

    feat: clear canvas

--- index.mjs
+++ index.mjs
@@ -1,4 +1,11 @@
-import { fromEvent, interval, map, merge, switchMap, takeUntil } from "./operators/index.mjs";
+import {
+  fromEvent,
+  interval,
+  map,
+  merge,
+  switchMap,
+  takeUntil,
+} from "./operators/index.mjs";
 import { formatPixelsToNumber } from "./utils/formatPixelsToNumber.mjs";
 
 const canvas = document.getElementById("canvas");
@@ -7,91 +14,133 @@ const clearBtn = document.getElementById("clear");
 const ctx = canvas.getContext("2d");
 
 const mouseEvents = {
-  down: 'mousedown',
-  move: 'mousemove',
-  up: 'mouseup',
-  leave: 'mouseleave',
+  down: "mousedown",
+  move: "mousemove",
+  up: "mouseup",
+  leave: "mouseleave",
 
-  touchstart: 'touchstart',
-  touchmove: 'touchmove',
-  touchend: 'touchend',
+  touchstart: "touchstart",
+  touchmove: "touchmove",
+  touchend: "touchend",
 
-  click: 'click',
-}
+  click: "click",
+};
 
 const getMousePosition = (canvasDom, eventValue) => {
-  const rect = canvasDom.getBoundingClientRect()
+  const rect = canvasDom.getBoundingClientRect();
   return {
     x: eventValue.clientX - rect.left,
     y: eventValue.clientY - rect.top,
-  }
-}
+  };
+};
 
 const resetCanvas = () => {
-  const canvasStyle = window.getComputedStyle(canvas)
-  const height = formatPixelsToNumber(canvasStyle.height)
-  const width = formatPixelsToNumber(canvasStyle.width)
+  const canvasStyle = window.getComputedStyle(canvas);
+  const height = formatPixelsToNumber(canvasStyle.height);
+  const width = formatPixelsToNumber(canvasStyle.width);
 
-  canvas.height = height
-  canvas.width = width
+  canvas.height = height;
+  canvas.width = width;
 
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-  ctx.lineWidth = 4
-}
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.lineWidth = 4;
+};
 
-resetCanvas()
+resetCanvas();
 
 const touchToMouse = (touchEvent, mouseEvent) => {
-  const [touch] = touchEvent.touches.length ?
-    touchEvent.touches :
-    touchEvent.changedTouches
+  const [touch] = touchEvent.touches.length
+    ? touchEvent.touches
+    : touchEvent.changedTouches;
 
   return new MouseEvent(mouseEvent, {
     clientX: touch.clientX,
     clientY: touch.clientY,
-  })
+  });
+};
 
-}
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+const store = {
+  db: [],
+  get() {
+    return this.db;
+  },
+  set(item) {
+    this.db.unshift(item);
+  },
+  clear() {
+    this.db.length = 0;
+  },
+};
 
 merge([
   fromEvent(canvas, mouseEvents.down),
-  fromEvent(canvas, mouseEvents.touchstart)
-    .pipeThrough(map(e => touchToMouse(e, mouseEvents.touchstart))),
+  fromEvent(canvas, mouseEvents.touchstart).pipeThrough(
+    map((e) => touchToMouse(e, mouseEvents.touchstart))
+  ),
 ])
   .pipeThrough(
-    switchMap(e => {
+    switchMap((e) => {
       return merge([
         fromEvent(canvas, mouseEvents.move),
-        fromEvent(canvas, mouseEvents.touchmove)
-          .pipeThrough(map(e => touchToMouse(e, mouseEvents.touchmove))),
-      ])
-        .pipeThrough(
-          takeUntil(merge([
+        fromEvent(canvas, mouseEvents.touchmove).pipeThrough(
+          map((e) => touchToMouse(e, mouseEvents.touchmove))
+        ),
+      ]).pipeThrough(
+        takeUntil(
+          merge([
             fromEvent(canvas, mouseEvents.up),
             fromEvent(canvas, mouseEvents.leave),
-            fromEvent(canvas, mouseEvents.touchend)
-              .pipeThrough(map(e => touchToMouse(e, mouseEvents.touchend))),
-          ]))
+            fromEvent(canvas, mouseEvents.touchend).pipeThrough(
+              map((e) => touchToMouse(e, mouseEvents.touchend))
+            ),
+          ])
         )
+      );
     })
-
   )
-  .pipeThrough(map(function ([mouseDown, mouseMove]) {
-    this._lastPosition = this._lastPosition ?? mouseDown
+  .pipeThrough(
+    map(function ([mouseDown, mouseMove]) {
+      this._lastPosition = this._lastPosition ?? mouseDown;
 
-    const [from, to] = [
-      this._lastPosition, mouseMove
-    ].map(item => getMousePosition(canvas, item))
+      const [from, to] = [this._lastPosition, mouseMove].map((item) =>
+        getMousePosition(canvas, item)
+      );
 
-    this._lastPosition = mouseMove.type === mouseEvents.up ? null : mouseMove
-    return { from, to }
-  }))
-  .pipeTo(new WritableStream({
-    write({ from, to }) {
-      ctx.beginPath()
-      ctx.moveTo(from.x, from.y)
-      ctx.lineTo(to.x, to.y)
-      ctx.stroke()
-      ctx.closePath()
-    }
-  }))
+      this._lastPosition = mouseMove.type === mouseEvents.up ? null : mouseMove;
+      return { from, to };
+    })
+  )
+  .pipeTo(
+    new WritableStream({
+      write({ from, to }) {
+        store.set({ from, to });
+        ctx.beginPath();
+        ctx.moveTo(from.x, from.y);
+        ctx.lineTo(to.x, to.y);
+        ctx.stroke();
+        ctx.closePath();
+      },
+    })
+  );
+
+// Clear canvas
+fromEvent(clearBtn, mouseEvents.click).pipeTo(
+  new WritableStream({
+    async write(chunk) {
+      ctx.beginPath();
+      ctx.strokeStyle = "white";
+
+      for (const { from, to } of store.get()) {
+        ctx.moveTo(from.x, from.y);
+        ctx.lineTo(to.x, to.y);
+        ctx.stroke();
+
+        await sleep(5);
+      }
+      resetCanvas(canvas.width, canvas.height);
+      store.clear();
+    },
+  })
+);
